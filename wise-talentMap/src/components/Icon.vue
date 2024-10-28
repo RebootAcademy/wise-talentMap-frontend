22f4f9c5c5509c3ad7f22f691e127a0f5678b358
RaulVillarraso <raulvillarraso@gmail.com>
2024-10-28 16:17:37 +0000

    - header structured - filter semi structured - custom button - custom input

---
+++ wise-talentMap/src/components/Icon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-html="iconComponentContent" aria-hidden="true"></div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true
+  }
+})
+
+const allowedIcons = {
+
+}
+
+const iconComponent = computed(() => {
+  return allowedIcons[props.icon] || null
+})
+
+const iconComponentContent = computed(() => {
+  return isSvg.value ? iconComponent.value : ''
+})
+</script>
--- wise-talentMap/src/layouts/MapLayout.vue
+++ wise-talentMap/src/layouts/MapLayout.vue
@@ -4,13 +4,18 @@
       <p>HEADER LOGOTYPE</p>
       <Button :clickFn="() => console.log('lolo')" class="h-11 text-2xl bg-softGray">ENTORNO VIRTUAL</Button>
     </div>
-    <div class="bg-white h-20 text-black">Filters</div>
+    <div class="bg-white h-20 text-black flex justify-between items-center">
+      <CustomInput icon="pi-search" placeholder="Buscar" class="w-20 h-8" v-model="searchParam" />
+      <Button class="border rounded-md w-24 gap-2.5"><span class="pi pi-filter-slash" />Filtros</Button>
+    </div>
     <RouterView />
   </div>
 </template>
 
 <script setup>
-import Button from '@/components/Button.vue'
+import Button from '@/components/CustomButton.vue'
+import CustomInput from '@/components/CustomInput.vue';
+import { ref } from 'vue';
 
-
+const searchParam = ref('')
 </script>
