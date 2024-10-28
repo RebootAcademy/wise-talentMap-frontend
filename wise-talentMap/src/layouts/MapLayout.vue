4b5dc5c4a2d0e18f57994de76a149c01f0bdab30
RaulVillarraso <raulvillarraso@gmail.com>
2024-10-28 15:20:21 +0000

    - header structured without 2 options

--- wise-talentMap/src/layouts/MapLayout.vue
+++ wise-talentMap/src/layouts/MapLayout.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="h-screen w-screen">
-    <div class="bg-blue-400 h-20 flex justify-between">
-      <p>LOGO</p>
-      <input />
-      <p>X</p>
+    <div class="bg-white border-b h-24 flex items-center justify-between font-bebas text-3xl text-black py-2.5 px-8">
+      <p>HEADER LOGOTYPE</p>
+      <Button :clickFn="() => console.log('lolo')" class="h-11 text-2xl bg-softGray">ENTORNO VIRTUAL</Button>
     </div>
-    <div class="bg-red-400 h-20">Filters</div>
+    <div class="bg-white h-20 text-black">Filters</div>
     <RouterView />
   </div>
 </template>
 
 <script setup>
+import Button from '@/components/Button.vue'
+
 
 </script>
