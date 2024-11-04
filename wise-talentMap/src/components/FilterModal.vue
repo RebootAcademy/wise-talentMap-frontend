27d7ede98f39a15037dcd552e231f551891bf422
RaulVillarraso <raulvillarraso@gmail.com>
2024-11-04 14:45:29 +0000

    - modal bordered

--- wise-talentMap/src/components/FilterModal.vue
+++ wise-talentMap/src/components/FilterModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <Dialog :visible="filtersVisible" class="w-[31rem]" modal dismissableMask>
+  <Dialog :visible="filtersVisible" class="w-[31rem]" style="border: 1px solid #881BF5" modal dismissableMask>
     <template #container>
       <div class="bg-white text-black rounded-md  flex flex-col gap-6">
         <header class="flex items-center justify-center gap-2 relative p-6 font-bebas text-2xl border-b">
