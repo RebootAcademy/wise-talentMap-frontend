1c648ac776bf709f840ac84788370be014c14735
Adasat <pbonbol@gmail.com>
2024-11-04 16:05:42 +0000

    Logo Added

--- wise-talentMap/src/components/Header.vue
+++ wise-talentMap/src/components/Header.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="flex items-center justify-between bg-blue-500 h-24  px-8">
-        <div>
-            ICONO
-        </div>
+              <img class="w-28 cursor-pointer" src="https://res.cloudinary.com/dcfgavulz/image/upload/v1730735936/logoTalentMap_vsxcz2.png" @click="() => router.push('/')">
+
         <div @click="$router.push('/statistics')">
             statistics
         </div>
     </div>
 </template>
+
+<script setup>
+import router from '@/router'
+
+</script>
