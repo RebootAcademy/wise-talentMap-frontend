a4a4642f20763cdf4cbdcde513ded4d5b4787d6b
Adasat <pbonbol@gmail.com>
2024-11-04 16:11:23 +0000

    Header sin Header

--- wise-talentMap/src/components/Header.vue
+++ wise-talentMap/src/components/Header.vue
@@ -1,14 +1,10 @@
 <template>
     <div class="flex items-center justify-between bg-blue-500 h-24  px-8">
-              <img class="w-28 cursor-pointer" src="https://res.cloudinary.com/dcfgavulz/image/upload/v1730735936/logoTalentMap_vsxcz2.png" @click="() => router.push('/')">
-
+        <div>
+            ICONO
+        </div>
         <div @click="$router.push('/statistics')">
             statistics
         </div>
     </div>
 </template>
-
-<script setup>
-import router from '@/router'
-
-</script>
