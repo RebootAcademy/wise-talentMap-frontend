e3fc40b3d5e9a44beb7e050ae073f39027901b03
RaulVillarraso <raulvillarraso@gmail.com>
2024-11-06 16:04:12 +0000

    - steam filters hidden on mobile view

--- wise-talentMap/src/layouts/MapLayout.vue
+++ wise-talentMap/src/layouts/MapLayout.vue
@@ -2,7 +2,9 @@
   <div class="h-screen w-full">
     <!-- Main header -->
     <div class="bg-white border-b h-24 flex items-center justify-between font-bebas text-3xl text-black py-2.5 px-8">
-      <img class="w-28 cursor-pointer" src="https://res.cloudinary.com/dcfgavulz/image/upload/v1730735936/logoTalentMap_vsxcz2.png" @click="() => router.push('/')">
+      <img class="w-28 cursor-pointer"
+        src="https://res.cloudinary.com/dcfgavulz/image/upload/v1730735936/logoTalentMap_vsxcz2.png"
+        @click="() => router.push('/')">
       <div class="flex gap-2">
         <CustomButton :clickFn="() => console.log('Wise')" class="h-11 text-2xl border border-black">
           WISE CANARIAS
@@ -13,7 +15,7 @@
       </div>
     </div>
     <!-- Filters bar -->
-    <div
+    <div v-if="checkWidth"
       :class="`bg-white h-20 text-black flex ${searchFocus ? 'gap-7' : 'justify-between '} items-center py-2.5 px-8`">
       <div class="w-full flex gap-7 items-center justify-center">
         <SteamFilterButtons />
@@ -33,6 +35,7 @@ import router from '@/router'
 const searchParam = ref('')
 const searchFocus = ref(false)
 const filtersVisible = ref(false)
+const checkWidth = ref(window.innerWidth > 678)
 
 const handleOpenModal = () => filtersVisible.value = !filtersVisible.value
 const onFocus = () => searchFocus.value = true
