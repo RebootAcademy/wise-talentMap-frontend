a3d6f3b8859c965e9d0cf636c5067367df70d5cc
RaulVillarraso <raulvillarraso@gmail.com>
2024-11-08 08:25:48 +0000

    - banner image convert to webp

--- wise-talentMap/src/components/Landing/LandingBanner.vue
+++ wise-talentMap/src/components/Landing/LandingBanner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-[url('/homePageBanner.jpeg')] bg-cover bg-right">
+    <div class="bg-[url('/homePageBanner.webp')] bg-cover bg-right">
         <div
             class="flex md:flex-row-reverse justify-center md:justify-start items-center md:min-h-[655px] bg-threeColorsBanner">
             <div class="md:w-1/2 px-0 md:px-12 lg:px-16 md:mr-12 py-12">
