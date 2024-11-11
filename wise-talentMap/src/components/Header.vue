69943264892a406c1f00584d312bb1e6fe530319
Adasat <pbonbol@gmail.com>
2024-11-11 10:41:44 +0000

    Resize the button to WiseCanarias

--- wise-talentMap/src/components/Header.vue
+++ wise-talentMap/src/components/Header.vue
@@ -2,19 +2,19 @@
   <div class="bg-white border-b h-24 flex items-center justify-between font-bebas text-3xl text-black py-2.5 px-8">
     <img class="w-28 cursor-pointer" @click="() => $router.push('/')"
       src="https://res.cloudinary.com/dcfgavulz/image/upload/v1730735936/logoTalentMap_vsxcz2.png" />
-    <div class="hidden lg:flex gap-2 items-center">
+    <div class=" lg:flex gap-2 items-center">
       <a href="https://www.wisecanarias.com" target="_blank">
-        <CustomButton isHovered class="h-11 text-2xl border rounded-md flex items-center border-black">
+        <CustomButton isHovered class="h-11 text-xl md:text-2xl border rounded-md flex items-center border-black">
           WISE CANARIAS
         </CustomButton>
       </a>
     </div>
-    <div class="lg:hidden">
+    <!-- <div class="lg:hidden">
       <CustomButton class="w-8 h-8 !p-0" :clickFn="toggleMenu">
         <Icon icon="burguer" size="!items-start" extraStyles="2rem" />
       </CustomButton>
       <Menu ref="showMenu" :model="items" popup />
-    </div>
+    </div> -->
   </div>
 </template>
 
