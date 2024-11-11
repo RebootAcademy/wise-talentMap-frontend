64f45370ce6fcb6013c4a4ce4d7b9b766b4d1019
Adasat <pbonbol@gmail.com>
2024-11-11 10:28:23 +0000

    Fixing Overflow, we can test in deploy site

--- wise-talentMap/src/views/LandingPage.vue
+++ wise-talentMap/src/views/LandingPage.vue
@@ -7,7 +7,6 @@
     <LandingVR class="col-span-4 sm:col-span-4 md:col-span-6 lg:col-span-12 " />
     <LandingGoals
       class="col-span-4 sm:col-span-4 md:col-span-6 lg:col-span-12 animate__animated animate__slideInLeft" />
-    <LandingTypeform class="col-span-4 md:col-span-6 lg:col-span-12" />
     <BannerMentions class="col-span-4 sm:col-span-4 md:col-span-6 lg:col-span-12" />
     <Footer class="col-span-4 md:col-span-6 lg:col-span-12"/>
     <ScrollToTop />
